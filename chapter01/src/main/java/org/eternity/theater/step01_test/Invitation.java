179490eb049bfcbde5c226f1262f020d6615ea33
epqlfepzk <epqlfepzk@naver.com>
2023-12-17 17:28:15 +0900

    file create

---
+++ chapter01/src/main/java/org/eternity/theater/step01_test/Invitation.java
@@ -0,0 +1,7 @@
+package org.eternity.theater.step01_test;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ chapter01/src/main/java/org/eternity/theater/step01_test/Ticket.java
@@ -0,0 +1,9 @@
+package org.eternity.theater.step01_test;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
